f709cd08b81185ed67af27385464c4cdeb6edc3f
Haroldoja <haroldoja@gmail.com>
2017-12-20 17:24:34 -0800

    My Django Girls app, second version

---
+++ blog/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+# Models created by HB Dec/20
+
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+	author = models.ForeignKey('auth.user', on_delete=models.CASCADE)
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
+	
+	def publish(self):
+		self.published.date = timezone.now()
+		self.save()
+		
+	def __str__(self):
+		return self.title
+		
+# class Post(models.Model): – this line defines our model (it is an object).
+# class is a special keyword that indicates that we are defining an object.
+# Post is the name of our model. We can give it a different name (but we must avoid 
+# special characters and whitespace). Always start a class name with an uppercase letter.
+# models.Model means that the Post is a Django Model, so Django knows that it 
+# should be saved in the database.
